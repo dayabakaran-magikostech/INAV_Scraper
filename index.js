04fa150b0e421ff1c690a1be60287efb1f26d4fb
dayabakaran-magikostech <karan.dayaba@magikostech.com>
2022-07-19 17:45:37 +0530

    index.js updated -> checking false request data

--- index.js
+++ index.js
@@ -10,19 +10,34 @@ const port = process.env.port;
 app.post('/fetch-inav-data', async function (req, res)
 {
 	console.log("Got a POST request to fetch inav data");
-	const sectors = Object.keys(req.body.urls); // getting keys from body > urls
-	const urls = Object.values(req.body.urls); // getting urls from body > urls
-	const result = await inavScrapper(sectors, urls); // invoking scrapper function by passing values and urls as arguments
-	//check result here
-	if(result)
+	if(req.body.urls)
 	{
-		res.status(200).send({msg:"Response Data" , result}); // sending response data from scrapper function
-		console.log("Response sent to page");
+		const sectors = Object.keys(req.body.urls); // getting keys from body > urls
+		const urls = Object.values(req.body.urls); // getting urls from body > urls
+		if(sectors.length >=1 && urls.length >= 1) // checking if any user has passed atleast 1 url
+		{
+			const result = await inavScrapper(sectors, urls); // invoking scrapper function by passing values and urls as arguments
+			if(result)
+			{
+				res.status(200).send({msg:"Response Data" , result}); // sending response data from scrapper function
+				console.log("Response sent to page");
+			}
+			else
+			{
+				res.status(500).send({msg:"Internal Server Error"});
+				console.log("Error occurred in inav scrapper");
+			}
+		}
+		else
+		{
+			res.status(400).send({msg:"Please pass an valid array"});
+			console.log("Please pass an valid array");
+		}
 	}
 	else
 	{
-		res.status(500).send({msg:"Internal Server Error"});
-		console.log("Error occurred in inav scrapper");
+		res.status(400).send({msg:"Please pass an array"});
+		console.log("Please pass an array");
 	}
 });
 
